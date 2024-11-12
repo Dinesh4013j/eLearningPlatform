7515e53d66758bce5bf343bfb0c178dd3378fa85
Hariharan4018 <140181589+Hariharan4018@users.noreply.github.com>
2024-11-12 21:40:09 +0530

    Update Layout.jsx

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -1,12 +1,6 @@
 import React from 'react'
 import {Outlet} from 'react-router-dom'
-import React from 'react';                  
-import { useState, useEffect } from 'react'; 
-import ReactDOM from 'react-dom';           
-import App from './App';       
-import './App.css';              
-import styles from './App.module.css'; 
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+
 
 
 function Layout() {
